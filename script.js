b56ae66aae069ef841d25550779217d1c65736cf
hankookswaram <98754301+hankookswaram@users.noreply.github.com>
2024-02-07 23:39:57 +0900

    commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function addItem() {
+    var inputElement = document.getElementById("input");
+    var inputValue = inputElement.value;
 
+    if (inputValue.trim() !== "") {
+        var checklistField = document.getElementById("checklist");
+        var checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.name = "item"
+        checkbox.value = inputValue;
+
+        var label = document.createElement("label");
+        label.appendChild(document.createTextNode(inputValue));
+        checklistField.appendChild(checkbox);
+        checklistField.appendChild(label);
+        checklistField.appendChild(document.createElement("br"));
+
+        inputElement.value = "";
+    }
+}
